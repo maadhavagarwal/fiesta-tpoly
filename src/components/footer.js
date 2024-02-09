60d3ae59176d076582e4f6fe29d5268782e0c6c5
maadhavagarwal <157884068+maadhavagarwal@users.noreply.github.com>
2024-02-09 11:03:54 +0530

    h

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import {Navbar,Row,Container,Nav} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
-
-
 function footer() {
   return (
 <Navbar bg="dark" variant='dark'>
       <Container>
         <LinkContainer to="/">
         
-        <div className='text-light bg-dark'>
+        <div className='text-light bg-dark navbar navbar-expand-lg navbar-dark'>
 
         <p>Jay Patel - 9313859293</p>
         <p>Dhanesh Shetty - 8591604650 </p>
         <p>Avaneesh Gawde - 9820874701 </p>
         <p>Hrishikesh Thakrar - 7020991887</p>
         <p>Email : iic@tpoly.in</p>
-        </div>
-        
-       
-             
-       
-        
+        </div> 
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
